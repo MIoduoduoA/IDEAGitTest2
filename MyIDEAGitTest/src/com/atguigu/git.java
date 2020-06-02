6c5bf6d98b29088f74a02f62f7eeb81dd65c2a86
tom_glb <goodMorning_glb@atguigu.com>
2020-06-02 11:16:14 +0800

    MyFirst Commit

---
+++ MyIDEAGitTest/src/com/atguigu/git.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author 陈志荣
+ * @create 2020--06--02--11:04
+ **/
+public class git {
+    public static void main(String[] args) {
+        System.out.println("推送数据到GitHub");
+    }
+}
